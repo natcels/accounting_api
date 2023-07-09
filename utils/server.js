9349ff36c7a2e2c478d44a663c4eae203b512fc0
Natcels <natcels@gmail.com>
2023-07-09 20:16:48 +0100

    updated security fils and app.js

--- utils/server.js
+++ utils/server.js
@@ -4,8 +4,8 @@ const app = require("./../app");
 const server = http.createServer(app);
 
 function startServer() {
-    port = process.env.PORT || 6000
-    server.listen(port, 6000);
+    port = process.env.PORT || 3000
+    server.listen(port);
     console.log("server started on port:", port);
 }
 
